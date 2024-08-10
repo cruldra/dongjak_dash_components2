301e09fdd7f81cde1c64e5a5e9e8d886dc0235e3
cruldra <cruldra@gmail.com>
2024-08-11 04:34:39 +0800

    移除 AdminAppLayout.tsx 中未使用的空行，以提高代码整洁度。
    #time 5m

--- src/ts/components/AdminAppLayout.tsx
+++ src/ts/components/AdminAppLayout.tsx
@@ -121,8 +121,6 @@ const AdminAppLayout: FC<AdminAppLayoutProps> = ({routes}) => {
         },
 
     ]);
-
-
     return <>
         <MantineProvider defaultColorScheme="auto">
             <RouterProvider router={router}/>
