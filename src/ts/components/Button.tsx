99218b8d76e6e870346a3ee37be95f7d323d9cb1
cruldra <cruldra@gmail.com>
2024-08-16 02:30:01 +0800

    feat: 添加 LoaderProps 和 NotificationProvider 组件
    #time 2h 36m

---
+++ src/ts/components/Button.tsx
@@ -0,0 +1,68 @@
+import {
+    Button as MantineButton,
+    MantineColor,
+    MantineGradient,
+    MantineRadius,
+    MantineSize,
+} from "@mantine/core";
+import { BoxProps } from "props/box";
+import { DashBaseProps } from "props/dash";
+import { LoaderProps } from "props/loader";
+import { StylesApiProps } from "props/styles";
+import React from "react";
+
+interface Props extends DashBaseProps, BoxProps, StylesApiProps {
+    /** Controls button `height`, `font-size` and horizontal `padding`, `'sm'` by default */
+    size?: MantineSize | `compact-${MantineSize}` | (string & {});
+    /** Key of `theme.colors` or any valid CSS color, `theme.primaryColor` by default */
+    color?: MantineColor;
+    /** Sets `justify-content` of `inner` element, can be used to change distribution of sections and label, `'center'` by default */
+    justify?: React.CSSProperties["justifyContent"];
+    /** Content displayed on the left side of the button label */
+    leftSection?: React.ReactNode;
+    /** Content displayed on the right side of the button label */
+    rightSection?: React.ReactNode;
+    /** Determines whether button should take 100% width of its parent container, `false` by default */
+    fullWidth?: boolean;
+    /** Key of `theme.radius` or any valid CSS value to set `border-radius`, `theme.defaultRadius` by default */
+    radius?: MantineRadius;
+    /** Gradient configuration used when `variant="gradient"`, default value is `theme.defaultGradient` */
+    gradient?: MantineGradient;
+    /** Indicates disabled state */
+    disabled?: boolean;
+    /** Button content */
+    children?: React.ReactNode;
+    /** Determines whether the `Loader` component should be displayed over the button */
+    loading?: boolean;
+    /** Props added to the `Loader` component (only visible when `loading` prop is set) */
+    loaderProps?: LoaderProps;
+    /** Determines whether button text color with filled variant should depend on `background-color`. If luminosity of the `color` prop is less than `theme.luminosityThreshold`, then `theme.white` will be used for text color, otherwise `theme.black`. Overrides `theme.autoContrast`. */
+    autoContrast?: boolean;
+    /** An integer that represents the number of times that this element has been clicked on */
+    n_clicks?: number;
+}
+
+/** Button */
+const Button = (props: Props) => {
+    const { children, setProps, disabled, n_clicks, ...others } = props;
+
+    const increment = () => {
+        if (!disabled) {
+            setProps({
+                n_clicks: n_clicks + 1,
+            });
+        }
+    };
+
+    return (
+        <MantineButton onClick={increment} disabled={disabled} {...others}>
+            {children}
+        </MantineButton>
+    );
+};
+
+Button.defaultProps = {
+    n_clicks: 0,
+};
+
+export default Button;
