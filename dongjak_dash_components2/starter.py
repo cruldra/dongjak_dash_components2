c6036c1b053bc8105252d5328abd350ff3531d66
cruldra <cruldra@gmail.com>
2024-08-15 22:17:41 +0800

    🚀 feat: 更新版本到 0.0.6，添加 black 依赖，修改 .gitignore 文件
    #time 57s

---
+++ dongjak_dash_components2/starter.py
@@ -0,0 +1,19 @@
+import dash
+from dash import Dash, _dash_renderer, html
+import dongjak_dash_components2 as ddc
+
+
+def function_testing_app(docs=None, inputs=None, outputs=None):
+    """
+    快速创建一个用于函数测试的Dash应用
+
+    :param docs: 文档字符串
+    :param inputs: 输入参数
+    :param outputs: 输出参数
+    """
+    _dash_renderer._set_react_version("18.2.0")
+    app = dash.Dash(__name__)
+    app.layout = ddc.MantineProvider(
+        ddc.FunctionCall(docs=docs, inputs=inputs, outputs=outputs)
+    )
+    return app
